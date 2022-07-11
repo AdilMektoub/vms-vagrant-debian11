543eceddfb767c46c3f104f5f9c65eeb475a1028
AdilMektoub <adil.mektoub@gmail.com>
2022-07-11 15:11:41 +0200

    delete script refactoring

--- command-in-master.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## install packages debian docke ansible
-
-IP=$(hostname -I | awk '{print $2}')
-
-echo "[1]: Joint the cluster"
-kubeadm init --apiserver-advertise-address=192.168.56.10 --pod-network-cidr=10.244.0.0/16
